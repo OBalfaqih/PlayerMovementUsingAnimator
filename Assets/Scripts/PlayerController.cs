6ba2914eb4ea645d7626cea61246cfe0eeef9854
Omar Balfaqih <obalfaqih@gmail.com>
2019-09-13 18:34:20 +0300

    Player Movement using Animator

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿/* 
+* Created by: Omar Balfaqih (@OBalfaqih)
+* http://obalfaqih.com
+*
+* Player Movement using Animator | Unity
+*
+* This script is for a tutorial on using Animator to create the player's movement
+*
+* Full tutorial available at:
+* https://www.youtube.com/watch?v=LofJMnWPClo&index=2&list=PLaqp5z-4pFi5auiUbsq_KChZKX-DufAOI
+*/
+
+/*
+* How to use:
+* 1- Attach this script to the player
+* 2- Assign the the player's Animator component to the "anim" variable
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Reference to the Animator component of the player
+    public Animator anim;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Link the "vertical" parameter in the animator to the player's vertical input
+        anim.SetFloat("vertical", Input.GetAxis("Vertical"));
+        // Link the "horizontal" parameter in the animator to the player's horizontal input
+        anim.SetFloat("horizontal", Input.GetAxis("Horizontal"));
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+<h1>Player Movement using Animator | Unity</h1>
+<p>This project is part of an online YouTube tutorial about using Animator Controller to move the player.</p>
+<p>
+	If you want to use your own animations, you'll need at least three animations:
+	<ul>
+		<li>Idle</li>
+		<li>Walk Forward</li>
+		<li>Turn Right or Left (Will mirror it to give us the second one)</li>
+	</ul>
+</p>
+<p>
+	<b>You can watch the full tutorial here:</b> https://www.youtube.com/watch?v=U0dlWhB_e0E
+</p>
+<p>Good luck and happy learning!</p>
